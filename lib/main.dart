e0f0825d2df01045586d7b38db2bfec9415df52d
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-14 23:57:51 +0530

    Add Function

--- lib/main.dart
+++ lib/main.dart
@@ -89,18 +89,23 @@ class HomeScreen extends StatelessWidget {
                         CategoryCard(
                           title: "Diet Recomendation",
                           svgSrc: "assets/icons/Hamburger.svg",
+                          press: (){},
                         ),
                         CategoryCard(
                           title: "Kegal Exercises",
                           svgSrc: "assets/icons/Excrecises.svg",
+                          press: (){},
                         ),
                         CategoryCard(
                           title: "Meditation",
                           svgSrc: "assets/icons/Meditation.svg",
+                          press: (){},
+
                         ),
                         CategoryCard(
                           title: "Yoga",
                           svgSrc: "assets/icons/yoga.svg",
+                          press: (){},
                         ),
                         
                       ],
@@ -119,45 +124,49 @@ class HomeScreen extends StatelessWidget {
 class CategoryCard extends StatelessWidget {
   final String svgSrc;
   final String title;
+  final Function press;
   const CategoryCard({
-    Key key, this.svgSrc, this.title,
+    Key key, this.svgSrc, this.title, this.press,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0,17),
-            blurRadius: 25,
-            spreadRadius: -20,
-            //color: kShadowColor,
-          ),
-        ],
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: (){},
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: <Widget>[
-                Spacer(),
-                SvgPicture.asset(svgSrc),
-                Spacer(),
-                Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context)
-                    .textTheme.title
-                    .copyWith(fontSize:15)
-                ),
-              ],
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        //padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0,17),
+              blurRadius: 25,
+              spreadRadius: -20,
+              //color: kShadowColor,
+            ),
+          ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: press,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  SvgPicture.asset(svgSrc),
+                  Spacer(),
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                      .textTheme.title
+                      .copyWith(fontSize:15)
+                  ),
+                ],
+              ),
             ),
           ),
         ),
